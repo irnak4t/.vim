ba8d7e3de7ce48d99852bbadddbb18a7d922185b
irnak4t <ima.tak0610@gmail.com>
2020-12-24 11:34:11 +0900

    Add plugin install script

---
+++ plugin.py
@@ -0,0 +1,21 @@
+import json
+import git
+import os
+
+bundle_json = open("bundle.json", "r")
+bundles = json.load(bundle_json)
+
+github_url = "https://github.com/"
+
+for provider, types in bundles.items():
+    if provider == "vendor":
+        for key, plugins in types.items():
+            for plugin in plugins:
+                plugin_url = github_url + plugin
+                plugin_name = plugin.split("/")[-1]
+                install_path = "./pack/vendor/" + key + "/" + plugin_name
+
+                print("Install " + plugin + "\n")
+                git.Repo.clone_from(
+                        plugin_url,
+                        install_path)
